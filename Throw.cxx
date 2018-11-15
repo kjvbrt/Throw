ef83379a4f3e25b918ea0ad06086e4f4c7731885
Juraj Smiesko <juraj.smiesko@cern.ch>
2018-11-15 15:46:13 +0100

    Separated Throw.cxx and ThrowTools.cxx

--- Throw.cxx
+++ Throw.cxx
@@ -39,101 +39,6 @@ using TMath::LocMax;
 using TMath::LocMin;
 
 
-// Tools
-// https://stackoverflow.com/questions/440133
-string Throw::randomString(size_t length) {
-  auto randchar = []() -> char {
-    const char charset[] =
-    "0123456789"
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz";
-    const size_t max_index = (sizeof(charset) - 1);
-    return charset[ rand() % max_index ];
-  };
-  string str(length, 0);
-  std::generate_n(str.begin(), length, randchar);
-
-  return str;
-}
-
-int Throw::GetMinimumIndex(TGraphAsymmErrors* graph,
-                           const string& param = "") {
-  unsigned int n = graph->GetN();
-  double* y = graph->GetY();
-
-  if (param.find("E") != string::npos) {
-    double y_err[n];
-    for (size_t i = 0; i < n; ++i) {
-      y_err[i] = y[i] - graph->GetErrorYlow(i);
-      cout << "y_err: " << y_err[i] << endl;
-    }
-    int minIndexErr = LocMin(n, y_err);
-
-    return minIndexErr;
-  }
-
-  int minIndex = LocMin(n, y);
-
-  return minIndex;
-}
-
-double Throw::GetMinimumX(TGraphAsymmErrors* graph,
-                          const string& param = "") {
-  int i = GetMinimumIndex(graph, param);
-  double x, y;
-  graph->GetPoint(i, x, y);
-
-  return x;
-}
-
-double Throw::GetMinimumY(TGraphAsymmErrors* graph,
-                          const string& param = "") {
-  int i = GetMinimumIndex(graph, param);
-  double x, y;
-  graph->GetPoint(i, x, y);
-
-  return y;
-}
-
-int Throw::GetMaximumIndex(TGraphAsymmErrors* graph,
-                           const string& param = "") {
-  unsigned int n = graph->GetN();
-  double* y = graph->GetY();
-
-  if (param.find("E") != string::npos) {
-    double y_err[n];
-    for (size_t i = 0; i < n; ++i) {
-      y_err[i] = y[i] + graph->GetErrorYhigh(i);
-    }
-    int minIndexErr = LocMax(n, y_err);
-
-    return minIndexErr;
-  }
-
-  int maxIndex = LocMax(n, y);
-
-  return maxIndex;
-}
-
-double Throw::GetMaximumX(TGraphAsymmErrors* graph,
-                          const string& param = "") {
-  int i = GetMaximumIndex(graph, param);
-  double x, y;
-  graph->GetPoint(i, x, y);
-
-  return x;
-}
-
-double Throw::GetMaximumY(TGraphAsymmErrors* graph,
-                          const string& param = "") {
-  int i = GetMaximumIndex(graph, param);
-  double x, y;
-  graph->GetPoint(i, x, y);
-
-  return y;
-}
-
-
 // Plotter
 int Throw::Plotter::nObj() {
   return histVec.size() + graphVec.size() + funcVec.size();
@@ -257,7 +162,7 @@ void Throw::Plotter::addGraph(TGraphAsymmErrors* inGraph) {
   graph->SetMarkerSize(.5);
 
   double graphMin = GetMinimumY(graph, "E");
-  double graphMinNoError = GetMinimumY(graph);
+  double graphMinNoError = GetMinimumY(graph, "");
   double graphMax = GetMaximumY(graph, "E");
 
   if (nObj() == 0) {
